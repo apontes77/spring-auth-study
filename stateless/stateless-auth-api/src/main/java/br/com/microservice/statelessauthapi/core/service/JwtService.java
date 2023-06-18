91d87f9764c359c951189ca78c8c32cfbcdb0433
Alexandre <alexandrepontesgyn@gmail.com>
2023-06-18 14:37:53 -0300

    feature: cria service que conectará endpoint à geração do token

--- stateless/stateless-auth-api/src/main/java/br/com/microservice/statelessauthapi/core/service/JwtService.java
+++ stateless/stateless-auth-api/src/main/java/br/com/microservice/statelessauthapi/core/service/JwtService.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
